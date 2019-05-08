796126912990c9652fa8fe1244c0a8630a644266
Nick Ebbitt <nickebbitt@gmail.com>
2019-05-08 16:15:56 +0100

    Refactor tests

--- src/test/java/org/neomatrix369/salaryslip/SalarySlipGeneratorShould.java
+++ src/test/java/org/neomatrix369/salaryslip/SalarySlipGeneratorShould.java
@@ -1,48 +1,34 @@
 package org.neomatrix369.salaryslip;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SalarySlipGeneratorShould {
 
+    private SalarySlip salarySlip;
+
+    @Before
+    public void setup() {
+        Employee employee = new Employee(12345, "John Doe", 24_000);
+        SalarySlipGenerator sut = new SalarySlipGenerator();
+        salarySlip = sut.generateFor(employee);
+    }
+
     @Test
     public void generateSalarySlipWithEmployeeID() {
-
-        final Employee employee = new Employee(12345, "John Doe", 24_000);
-
-        final SalarySlipGenerator sut = new SalarySlipGenerator();
-        SalarySlip salarySlip = sut.generateFor(employee);
-
         assertEquals(12345, salarySlip.employeeID());
-
     }
 
     @Test
     public void generateSalarySlipWithEmployeeName() {
-
-        final Employee employee = new Employee(12345, "John Doe", 24_000);
-
-        final SalarySlipGenerator sut = new SalarySlipGenerator();
-        SalarySlip salarySlip = sut.generateFor(employee);
-
-        assertEquals(12345, salarySlip.employeeID());
         assertEquals("John Doe", salarySlip.employeeName());
-
     }
 
     @Test
     public void generateSalarySlipWithGrossMonthlySalary() {
-
-        final Employee employee = new Employee(12345, "John Doe", 24_000);
-
-        final SalarySlipGenerator sut = new SalarySlipGenerator();
-        SalarySlip salarySlip = sut.generateFor(employee);
-
-        assertEquals(12345, salarySlip.employeeID());
-        assertEquals("John Doe", salarySlip.employeeName());
         assertEquals(2_000, salarySlip.grossMonthlySalary());
-
     }
 
 
